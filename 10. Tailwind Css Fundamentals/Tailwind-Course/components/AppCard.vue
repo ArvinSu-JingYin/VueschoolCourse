54f41374382c7ab188c5d76719e867ddcec0f25e
MSI\itjin <it.jing.yin@gmail.com>
2024-09-23 00:06:49 +0800

    feat: Add main navigation and form components
    Added TheMainNav, AppButton, and AppInput components, along with related styles and dependency updates.

--- 10. Tailwind Css Fundamentals/Tailwind-Course/components/AppCard.vue
+++ 10. Tailwind Css Fundamentals/Tailwind-Course/components/AppCard.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    <img src="~/assets/pic/cat.jpg" alt="cat" />
+  <div class="shadow-md p-5 rounded-md bg-gray-100">
+    <img class="rounded-md mb-4 mx-auto" src="~/assets/pic/cat.jpg" alt="cat" />
+    <h4 class="font-extralight text-2xl">Cat Picture</h4>
+    <hr class="bg-gray-400 my-2 h-[2px]" />
+    <p class="line-clamp-3">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit delectus,
+      animi unde quas necessitatibus vitae. Aperiam accusantium asperiores, iure
+      eligendi ipsum reprehenderit quidem molestias ad, facere quo tempora at
+      consequatur!
+    </p>
   </div>
 </template>
 
---
+++ 10. Tailwind Css Fundamentals/Tailwind-Course/components/TheMainNav.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul>
+    <li>
+      <NuxtLink
+        to="/idontknow"
+        class="visited:text-purple-400 focus:outline-purple-700"
+        >Home</NuxtLink
+      >
+    </li>
+    <li>
+      <NuxtLink to="/about">About</NuxtLink>
+    </li>
+    <li>
+      <NuxtLink to="/contact">Contact</NuxtLink>
+    </li>
+  </ul>
+</template>

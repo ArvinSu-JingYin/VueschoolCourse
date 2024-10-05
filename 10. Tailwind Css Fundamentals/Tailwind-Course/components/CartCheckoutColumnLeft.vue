13adc2d25c28e00a48cbfb00bb5e05d5072c4eb0
MSI\itjin <it.jing.yin@gmail.com>
2024-10-06 00:05:00 +0800

    refactor: Update tsconfig.json and CartCheckoutColumnLeft.vue

--- 10. Tailwind Css Fundamentals/Tailwind-Course/components/CartCheckoutColumnLeft.vue
+++ 10. Tailwind Css Fundamentals/Tailwind-Course/components/CartCheckoutColumnLeft.vue
@@ -1,13 +1,24 @@
 <template>
-  <div>
-    
+  <div class="relative col-span-3">
+    <!-- Computer Image -->
+     <img src="/assets/pic/Notebook.jpg" 
+          alt=""
+          class="object-cover object">
+    <!-- Price -->
+     <div class="absolute right-0 z-10 bottom-10">
+      <div>
+        <span>Total:</span>
+        <div>
+          <span>$1,999.00</span>
+          <span>In Stock | Free Shipping</span>
+        </div>
+      </div>
+<!-- Circles -->
+ <div>
+  <div></div>
+  <div></div>
+  <div></div>
+ </div>
+     </div>
   </div>
 </template>
-
-<script lang="ts" setup>
-
-</script>
-
-<style>
-
-</style>
